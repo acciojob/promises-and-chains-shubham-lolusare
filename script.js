d821c202048e8a790a10567f4eb9507c86ad06cd
Shubham Lolusare <shubhamlolusare@gmail.com>
2023-03-29 18:34:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+let form = document.getElementById("data-form");
+let name = document.getElementById("name");
+let age = document.getElementById("age");
+
+
+form.addEventListener("submit",(event)=>{
+	event.preventDefault();
+	evaluate(name.value , age.value)
+		.then((data)=>{
+			alert(`Welcome, ${data}. You can vote`);
+		},(error)=>{
+			alert(`Oh sorry ${error}. You aren't old enough.`);
+		})
+});
+
+
+function evaluate(name , age){
+	return new Promise((resolve,reject)=>{
+		if(age >= 18){
+			setTimeout(()=>{resolve(name);},4000)
+			
+		}else{
+			reject(name);
+		}
+	})
+}
