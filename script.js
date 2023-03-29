a1a09b393a914d04108bfe6f11d15522d99ef96c
Shubham Lolusare <shubhamlolusare@gmail.com>
2023-03-29 18:52:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ btn.addEventListener("click",(event)=>{
 		.then((data)=>{
 			alert(`Welcome, ${data}. You can vote.`);
 		},(error)=>{
-			alert(`Oh sorry ${error}. You aren't old enough.`);
+			alert(`Oh sorry ${error}. You aren't old enough.`+"\nPlease enter valid details");
 		})
 });
 
